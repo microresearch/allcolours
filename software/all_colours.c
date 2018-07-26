be96dcdcbd9c4a71f2038d802d259233d6ef0d72
root <root@xxxxx>
2018-07-26 14:52:21 +0200

    xx

--- software/all_colours.c
+++ software/all_colours.c
@@ -4,9 +4,9 @@ NEW TESTBED as not compatible with ERD/SIR
 
 TODO: do we need ISR to update OCR/PWM thing or can we update it anytime?
 
-- leaky or mutating shift register and/or control of 7400
+- leaky or mutating shift register and/or control of 7490
 
-- pulse out, primitive DAC out, clock out to 7400
+- pulse out, primitive DAC out, clock out to 7490
 
 - CV in incoming bit or set of bits to shift and/or value for clock out, CV pulse in to lock/unlock, slew for clock out/cv in, extra as speed of shifting?
 - pulse in to shift... purpose of shifting ...
@@ -16,21 +16,27 @@ TODO: do we need ISR to update OCR/PWM thing or can we update it anytime?
 start to list modes:
 
 1-> various shift register implementations in pulse/bit and speed as
-speed/skew CV/knob - OUT is pulse/bit and/or DAC out and OUT is 7400
+speed/skew CV/knob - OUT is pulse/bit and/or DAC out and OUT is 7490
 clock/PWM OUT from shift reg taps ...
 
 2-> various shift register implementations in as pulse/bit (from CV -
 threshold) and speed as speed/skew CV/knob - OUT is pulse/bit and/or
-DAC out and in CV controls 7400 clock/PWM OUT and is skewed by
+DAC out and in CV controls 7490 clock/PWM OUT and is skewed by
 speed/skew CV/knob
 
 3-> various shift register implementations in pulse/bit and speed as
 speed/skew CV/knob - OUT is pulse/bit and/or DAC out and in pulse
-controls 7400 clock/PWM out
+controls 7490 clock/PWM out
 
 other modes?
 
-we can xor and other ops CV and shift reg/pulse/DAC to 7400 CLOCK
+we can xor and other ops CV and shift reg/pulse/DAC to 7490 CLOCK
+
+////
+
+- pulse in, clock in (interrupt INT0 or?), (multiple in pulses?), output pulses for each SR stage...
+
+- basic 16 stage shift register - left shifts and its speed
 
  */
 
@@ -55,6 +61,23 @@ typedef unsigned char u8;
 #define cbi(reg, bit) reg &= ~(BV(bit)) // Clears the corresponding bit in register reg
 #define sbi(reg, bit) reg |= (BV(bit))              // Sets the corresponding bit in register reg
 
+#define PinVal(Port, Bit, Val)    Port = Val ? Port | (1 << Bit) : Port & ~(1 << Bit);
+
+uint16_t start_state = 0xACE1u;  /* Any nonzero start state will work. */
+uint16_t lfsr = 0xACE1u;
+uint16_t bit;                    /* Must be 16bit to allow bit<<15 later in the code */
+unsigned char biit=0x05;
+
+// interrupt for clock IN - rising edge on INT0 - pin PD2, pin 2 on Arduino UNO
+
+ISR(INT0_vect)
+{
+  // shift it through and output
+  bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+  lfsr =  (lfsr >> 1) | (bit << 15);
+  // bit on pin PD3
+  PinVal(PORTD, PD3, bit);
+}
 
 // set up adc, inputs, outputs and PWM out
 
@@ -75,7 +98,8 @@ void pwm_init(void){ // pwm TEST! - from original microbdinterp but there we cha
   // 16 bit counter? - PB1 pin 1 on PORT B = pin 13 OC1A
   // what range we can get?
   TCCR1A= (1<<COM1A0);// | (1<<WGM11) | (1<<WGM10); // KEEP AS CTC for filter
-  TCCR1B= (1<<WGM12) | (1<<CS11);// divide by 8 which gives 1MHz
+  //  TCCR1B= (1<<WGM12) | (1<<CS11);// divide by 8 which gives 1MHz == 10KHz for filter so maybe keep it higher/no divide... or different modes
+  TCCR1B= (1<<WGM12) | (1<<CS10); // no divider
   OCR1A=100; // ?? 16 bits?
 }
 
@@ -83,8 +107,15 @@ void io_setup(void){
   DDRC = 0x00;
   PORTC = 0x00; // for ADC above
 
-  // pwm out??, pulse in, pulse out and DAC out
-  
+  // pwm out??, pulse in, pulse out 
+
+  // for pulse in 
+
+  //  DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
+  DDRD = 0b00001010;
+  EICRA |= ((1 << ISC01) | (1 << ISC00)); // Rising edge of INT0 will fire the ISR - this is for clock in
+  EIMSK |= (1 << INT0); // Enable INT0 interrupt, will call INT0_vect ISR when INT0 fires
+  sei();               // Enable global interrupts
 }
 
 unsigned char adcread(unsigned char channel){
--- software/test.c
+++ software/test.c
@@ -12,15 +12,18 @@ int main(void)
     uint16_t bit;                    /* Must be 16bit to allow bit<<15 later in the code */
     unsigned period = 0;
     unsigned char bat;
+    unsigned char biit=0x05;
+
+    //    if (!(biit & 0x20)) printf("BIT\n");
     
-    do
+       do
     {
         /* taps: 16 14 13 11; feedback polynomial: x^16 + x^14 + x^13 + x^11 + 1 */
         bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
-	// slipped bit based on what kind of random???
-	if (bit==1)	printf("x");
-	else printf("0");
-	lfsr =  (lfsr >> 1) | (bit << 15);
+    	// slipped bit based on what kind of random???
+    	if (bit==1)	printf("x");
+    	else printf("0");
+    	lfsr =  (lfsr >> 1) | (bit << 15);
         ++period;
     } while (1);
 
