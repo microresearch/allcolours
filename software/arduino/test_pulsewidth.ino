fed36b2c315453b6c9c1a5d0266ec6eb9b895efc
root <root@xxxxx.xxxxx>
2019-07-18 18:47:01 +0200

    xx

--- software/arduino/test_pulsewidth.ino
+++ software/arduino/test_pulsewidth.ino
@@ -1,4 +1,4 @@
-//#define F_CPU 16000000UL 
+b//#define F_CPU 16000000UL 
 
 #include <stdio.h>
 #include <stdint.h>
@@ -18,8 +18,20 @@ int x=0, y=0;
 
 u8 testing[16]={160,800,128,255,128,250,770,180,290,299,313,230,444,222,110,450};
 
-// Interrupt is called once a millisecond, 
-SIGNAL(TIMER0_COMPA_vect) 
+#define PinVal(Port, Bit, Val)    Port = Val ? Port | (1 << Bit) : Port & ~(1 << Bit);
+
+uint16_t start_state = 0xACE1u;  /* Any nonzero start state will work. */
+uint16_t lfsr = 0xACE1u;
+uint16_t bit;                    /* Must be 16bit to allow bit<<15 later in the code */
+
+ISR(INT0_vect) // doesn't run faster than 444 KHz
+
+{
+  //  PORTB=1; //pin 8 PB0
+  //    PORTB=0;
+}
+
+SIGNAL(TIMER0_COMPA_vect) // again around 444 KHz speed 
 {
   /*  y++;
   if (y==160) {
@@ -30,17 +42,27 @@ SIGNAL(TIMER0_COMPA_vect)
   }*/
 
   // test toggle pin 8 PB0
-  PORTB=1; // in main loop this gives us 4 MHz
-  PORTB=0;
-  OCR0A = 0x20; // 0xOF should be 1 MHz - but fastest we go is 444 Khz = 4K filter  
+  //    PORTB=1; // in main loop this gives us 4 MHz
+  //    PORTB=0;
+  OCR0A = 0x01; // 0xOF should be 1 MHz - but fastest we go is 444 Khz = 4K filter  
+
+  bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+  lfsr =  (lfsr >> 1) | (bit << 15);
+  //  PinVal(PORTB, 0, bit); // pin 3 on arduino / basic SR out with no IN...
+  // or bit as very short pulse is nicer!
+  if (bit==1) {
+    PORTB=1; 
+    PORTB=0;
+  }    
+  
 }
 
 void setup() {
   pinMode(0,OUTPUT); //  we always need these
-  pinMode(9,OUTPUT); 
+  pinMode(7,OUTPUT); 
   pinMode(8,OUTPUT); 
+  pinMode(9,OUTPUT); 
   pinMode(11,OUTPUT);
-    // for pin
     
   cli();//stop interrupts
 
@@ -49,15 +71,13 @@ void setup() {
   // OCR1A is the fast one - 16 bit CTC!
   TCCR1A= (1<<COM1A0);// | (1<<WGM11) | (1<<WGM10); // KEEP AS CTC for filter
   TCCR1B= (1<<WGM12) | (1<<CS10);// no pre-scale
-  OCR1A=32; //0=8 MHz // start at 8 for around 1MHz=10K filter
+  OCR1A=64; // 0=8 MHz // start at 8 for around 1MHz=10K filter
 
-// interrupt at 1KHz to change pwm = timer0
-  TCNT0=0;
-  //  OCR0A = 0x01; // 0xOF should be 1 MHz 
-  ///  TCCR0B= (1<<CS01) | (1<<CS00); // 64 divisor = 1 millisecond
-  TCCR0A= (1<<WGM01); // CTC mode clear on compared match 
-  TCCR0B= (1<<CS00); // no divider
-  TIMSK0 |= _BV(OCIE0A); // enable timer0 overflow interrupt
+		//interrupt at 1KHz to change pwm = timer0
+    TCNT0=0;
+    TCCR0A= (1<<WGM01); // CTC mode clear on compared match
+    TCCR0B= (1<<CS00); // no divider
+    TIMSK0 |= _BV(OCIE0A); // enable timer0 overflow interrupt
 
   
     // 1024= 7 Khz= 7000/100=70Hz 1khz-10Hz in the filter
@@ -68,8 +88,13 @@ void setup() {
   //    TCCR2A = _BV(COM2A0) | _BV(WGM21) | _BV(WGM21);
   //    TCCR2B=(1<<CS21) | (1<<CS20);  // divide by 64 for 250KHz
   //    OCR2A=1;
-    
-  sei();//allow interrupts
+
+  // set up interrupt INT0 on pin PD2 which is arduino pin 2
+
+    EICRA |= ((1 << ISC01) | (1 << ISC00)); // Rising edge of INT0 will fire the ISR - this is for clock in
+    EIMSK |= (1 << INT0); // Enable INT0 interrupt, will call INT0_vect ISR when INT0 fires
+  
+      sei();//allow interrupts
 }
 
 void loop() {
@@ -79,10 +104,11 @@ void loop() {
    //  OCR1A=x;
    //   delay(10);
 
-  //  PORTD=0; // in main loop this gives us 4 MHz - ON
-  //   _delay_us(100); // with 1 uS delay we get 440 KHz
+  PORTD=255; // in main loop this gives us 4 MHz - ON
+  // with fast timer interrupt this drops to 119 KHz
   //  _delay_ms(1000);
-  //  PORTD=255; // off
+  PORTD=0; // off
+  //  _delay_us(1); // with 1 uS delay we get 440 KHz
    //   _delay_us(1000000);
   //     _delay_ms(1000);
 
