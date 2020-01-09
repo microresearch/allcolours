511601be409cad26074644fa0cae0db8beda7c21
root <root@xxxxx.xxxxx>
2020-01-09 18:15:26 +0100

    x60

--- log_gen.py
+++ log_gen.py
@@ -1,12 +1,16 @@
 import numpy
 
 starter=312
-ender=(starter*33)
+ender=(starter*17)
 
 #print ender
 
 xx=numpy.logspace(0, 5, num=1024, endpoint=True, base=2.0)
 #file = open('/root/testfile', 'w')
-for xyz in reversed(xx):
- #   file.write("%s, " % int(xyz))
-    print("%s," % int(ender-(starter*xyz))),
+for xyz in xx:
+#    file.write("%s, " % int(xyz))
+#    print("%s," % int(starter*xyz)),
+    print("%s," % int(xyz)),
+#    print("%s," % int(starter*xyz)),
+#for xyz in xx:
+ #    print("%s," % int(xyz)),
