7bbabb59e89720823bf18f3ade0637cdf1d4805d
root <root@xxxxx.monl>
2020-01-22 18:52:36 +0100

    also new experimental modes

--- log_gen.py
+++ log_gen.py
@@ -9,8 +9,8 @@ xx=numpy.logspace(0, 5, num=1024, endpoint=True, base=2.0)
 #file = open('/root/testfile', 'w')
 for xyz in xx:
 #    file.write("%s, " % int(xyz))
-#    print("%s," % int(starter*xyz)),
-    print("%s," % int(xyz)),
+    print("%s," % int(1+(((starter*xyz)-312)/32))),
+    #    print("%s," % int(xyz)),
 #    print("%s," % int(starter*xyz)),
 #for xyz in xx:
  #    print("%s," % int(xyz)),
