bdc94a3a40c015b0c134c530c0b6a5cf80a5ab52
root <root@xxxxx.monl>
2020-02-13 22:26:20 +0100

    log_gen

---
+++ log_gen2.py
@@ -0,0 +1,11 @@
+import numpy
+
+xx=numpy.logspace(0, 14, num=1024, endpoint=True, base=2.0) # 512 of log to 1024 (2^10)
+#file = open('/root/testfile', 'w')
+for xyz in xx:
+#    file.write("%s, " % int(xyz))
+#    print("%s," % int(starter*xyz)),
+    print("%s," % int(xyz-1)),
+#    print("%s," % int(starter*xyz)),
+#for xyz in xx:
+ #    print("%s," % int(xyz)),
