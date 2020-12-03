4c4d862dfe6f525e252a298a4c128342531ba3f8
root <root@xxxxx.xxxxx>
2020-12-03 18:44:09 +0100

    final fixes for SR log speed and low mode 47, minor interpol fixes

--- log_gen2.py
+++ log_gen2.py
@@ -1,6 +1,6 @@
 import numpy
 
-xx=numpy.logspace(0, 14, num=1024, endpoint=True, base=2.0) # 512 of log to 1024 (2^10)
+xx=numpy.logspace(0, 18, num=1024, endpoint=True, base=2.0) # 512 of log to 1024 (2^10)
 #file = open('/root/testfile', 'w')
 for xyz in xx:
 #    file.write("%s, " % int(xyz))
