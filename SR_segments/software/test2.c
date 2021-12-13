ba877390f81d4a5a77992aeed532bb6149758e8e
root <root@xxxxx.monl>
2021-12-13 13:21:09 +0100

    xx

--- SR_segments/software/test2.c
+++ SR_segments/software/test2.c
@@ -27,9 +27,11 @@ uint32_t shift_[4], Gshift_[4][4], GGshift_[4], cogg[4][4], route[4]; // gshift 
 // with 256 cycles/copies - or we can have variable length of this shifting array
 
 uint32_t Gshift_rev[4][256], Gshift_revcnt[4]={0,0,0,0};
-uint32_t default_route[4]={3,0,1,2}; // for 0,1,2,3 N,L,C,R - but routing is routing FROM!
+uint32_t default_route[4]={8,1,2,4}; // for 0,1,2,3 N,L,C,R - but routing is routing FROM!
 uint32_t single_route[4]={1,2,4,8};
 uint32_t sroute=0; // counter for single routes so we can share this
+
+uint32_t neworder[4]={3,2,1,0};
 
 uint32_t *speedroute[4][5]; // speed from our CV, CV as offset for DAC from (1,2,3,4) // just for speedCV modes - pointer to these
 
@@ -314,13 +316,13 @@ int main(void)   // try to re-learn pointer arrays
   //  uint32_t *speedroute[4][5]; // speed from our CV, CV as offset for DAC from (1,2,3,4) // just for speedCV modes - so could be pointer to these
   // but we can't add offset in pointers?
   speedroute[0][0]=&speedn; // eg CV speed.
-  speedn=253;
+  speedn=1;
   
   uint32_t b, g, x, y, clkr=0;
   shift_[0]=0b1010101010101010101010101010101;
   shift_[1]=0b0110111011111011101110111110000;
-  shift_[2]=0b0110111011111011101110111110000;
-  shift_[3]=0b0110111011111011101110111110000;
+  shift_[2]=0b0110111011111011101110111110010;
+  shift_[3]=0b0110111011111011101110111110010;
   
   // seed rand
   srand (time(NULL));
@@ -337,68 +339,90 @@ int main(void)   // try to re-learn pointer arrays
   // whole of SR should be configurable as stream of bits so we can read a description from another SR
   // also mode and pulses can modify bits
   ////////////////////////////////////////////////////
+
+  uint32_t c=0;
   
-  b=0; //this is us
+  for (y=0;y<16;y++){
+
+  if (c>3) {
+    c=0;
+    printf("\n");
+    // they run at different speeds though so we cant do this///
+    // what can we do?
+/*
+    Gshift_[0][0]=shift_[0]; 
+    Gshift_[0][1]=shift_[0]; 
+    Gshift_[0][2]=shift_[0];
+    Gshift_[0][3]=shift_[0]; // ghosts for l,c,r only but let's keep one spare
+
+    Gshift_[1][0]=shift_[1]; 
+    Gshift_[1][1]=shift_[1]; 
+    Gshift_[1][2]=shift_[1];
+    Gshift_[1][3]=shift_[1]; // ghosts for l,c,r only but let's keep one spare
+
+    Gshift_[2][0]=shift_[2]; 
+    Gshift_[2][1]=shift_[2]; 
+    Gshift_[2][2]=shift_[2];
+    Gshift_[2][3]=shift_[2]; // ghosts for l,c,r only but let's keep one spare
+
+    Gshift_[3][0]=shift_[3]; 
+    Gshift_[3][1]=shift_[3]; 
+    Gshift_[3][2]=shift_[3];
+    Gshift_[3][3]=shift_[3]; // ghosts for l,c,r only but let's keep one spare      
+*/
+  }
+  b=neworder[c];
   // we want speed to be a route:
   //    countern++;
   //  if (countern>=speedn){ 
   //    countern=0;
   //  printf("spedrot: %d\n", *speedroute[b][0]); // this works
   counter[b]++;
-  if (counter[b]>*speedroute[b][0]){ // speed from our CV, CV as offset for DAC from (1,2,3,4) // just for speedCV modes - 0 is route
+  //  if (counter[b]>*speedroute[b][0]){ // speed from our CV, CV as offset for DAC from (1,2,3,4) // just for speedCV modes - 0 is route
+  if (counter[b]>0){ // speed from our CV, CV as offset for DAC from (1,2,3,4) // just for speedCV modes - 0 is route
     // how to make speed offset generic pointer (not) except using if ?
     counter[b]=0;
   // trial routing table
   // 6/9/2021 - important thing is if we have routing table then what do we do if is
   //  zeroes for one route - we need always to preserve a default route
 
-  route[b]=1; // lowest 4 bits of routing NLCR 1000 so C would be 0010 = 2  - or we use binary weighted
-
-  for (y=0;y<32;y++){
+    route[b]=default_route[b]; // lowest 4 bits of routing NLCR 1000 so C would be 0010 = 2  - or we use binary weighted
+    Gshift_[b][0]=shift_[b]; 
+    Gshift_[b][1]=shift_[b]; 
+    Gshift_[b][2]=shift_[b];
+    Gshift_[b][3]=shift_[b]; // ghosts for l,c,r only but let's keep one spare
+    
   
   //  g=2; // n=0,l=1,c=2,r=3 // so feedback here is from c 
-  bitn = ((shift_[b] >> (lfsr_taps[SRlength[b]][0])) ^ (shift_[b] >> (lfsr_taps[SRlength[b]][1])) ^ (shift_[b] >> (lfsr_taps[SRlength[b]][2])) ^ (shift_[b] >> (lfsr_taps[SRlength[b]][3]))) & 1u; // 32 is 31, 29, 25, 24
+    //  bitn = ((shift_[b] >> (lfsr_taps[SRlength[b]][0])) ^ (shift_[b] >> (lfsr_taps[SRlength[b]][1])) ^ (shift_[b] >> (lfsr_taps[SRlength[b]][2])) ^ (shift_[b] >> (lfsr_taps[SRlength[b]][3]))) & 1u; // 32 is 31, 29, 25, 24
   
   // copy now to ghost
   //we need multiple ghosts for each possible shifter: eg. Gshift_rl, Gshift_rn, Gshift_rc (right ones for left, for n and for c)
-  Gshift_[b][0]=shift_[b]; 
-  Gshift_[b][1]=shift_[b]; 
-  Gshift_[b][2]=shift_[b];
-  Gshift_[b][3]=shift_[b]; // ghosts for l,c,r only but let's keep one spare
 
   // what would ghosts in ghost look like... only for itself
   // and again ghost shift can occur on pulses in so is delayed rev...
   // which is the only way it makes sense
   // events for copy in, for shift and/or for bit extraction
-
   // if event {
-  GGshift_[b]=Gshift_[b][b];
+  //  GGshift_[b]=Gshift_[b][b];
   // if event {
-  bitr= (GGshift_[b]>>SRlength[b]) & 0x01;
-  GGshift_[b]=(GGshift_[b]<<1)+bitr;
+  //  bitr= (GGshift_[b]>>SRlength[b]) & 0x01;
+  //  GGshift_[b]=(GGshift_[b]<<1)+bitr;
   // } wrap
-
+  
   // alternative for array of revenants:
   //uint32_t Gshift_rev[4][256], Gshift_revcnt[4]={0,0,0,0};
   // if event {
-  Gshift_rev[b][Gshift_revcnt[b]]=Gshift_[b][b];
+  //  Gshift_rev[b][Gshift_revcnt[b]]=Gshift_[b][b];
   // if event {
-  bitr= (Gshift_rev[b][Gshift_revcnt[b]]>>SRlength[b]) & 0x01;
-  Gshift_rev[b][Gshift_revcnt[b]]=(Gshift_rev[b][Gshift_revcnt[b]]<<1)+bitr;
-  Gshift_revcnt[b]++; // events for increment
-  if (Gshift_revcnt[b]>255) Gshift_revcnt[b]=0;
+  //  bitr= (Gshift_rev[b][Gshift_revcnt[b]]>>SRlength[b]) & 0x01;
+  //  Gshift_rev[b][Gshift_revcnt[b]]=(Gshift_rev[b][Gshift_revcnt[b]]<<1)+bitr;
+  //  Gshift_revcnt[b]++; // events for increment
+  //  if (Gshift_revcnt[b]>255) Gshift_revcnt[b]=0;
   // } wrap
-  
     // need to catch it
-  if (shift_[b]==0)     shift_[b]=0xff;
-  
+  //  if (shift_[b]==0)     shift_[b]=0xff;
   // what else it needs to know - for each queued SR we need one cogg=0 for reset as we reset each on reading
-  // if we are using coggs to move on registers
-  // this just signals that we have a reset for SR down the line
-  cogg[b][0]=0;
-  cogg[b][1]=0;
-  cogg[b][2]=0;
-  cogg[b][3]=0;
 
   shift_[b]=shift_[b]<<1;
   
@@ -412,27 +436,28 @@ int main(void)   // try to re-learn pointer arrays
   // if route to ourself then is cycling bit but we still need to cycle <<1 above!
   for (x=0;x<4;x++){
     if (tmp&0x01){  
-      bitrr = (Gshift_[b][x]>>SRlength[x]) & 0x01; // or other logical opp for multiple bits/accum
+      bitrr = (Gshift_[x][b]>>SRlength[x]) & 0x01; // or other logical opp for multiple bits/accum
       // could even shift them in one after the other
       // ghosts can also have ghosts and LFSR within themselves
-      Gshift_[b][x]=(Gshift_[b][x]<<1)+bitrr; // but this doesn't make sense as then later ones - TOFIX/FIXED
+      Gshift_[x][b]=(Gshift_[x][b]<<1)+bitrr; // but this doesn't make sense as then later ones - TOFIX/FIXED
       bitr |= bitrr;
     }
     tmp=tmp>>1;
   }
 
+  
   // check that if is only a routing to itself that we do have one default route bit
-  if (route[b]==(1<<b)) { // only itself - add in default route which is?
-    x=default_route[b];
-    bitr |= (Gshift_[b][x]>>SRlength[x]) & 0x01; // or other logical opp for multiple bits/accum
-    Gshift_[b][x]=(Gshift_[b][x]<<1)+bitr;
-  } 
-    
+//    if (route[b]==(1<<b)) { // only itself - add in default route which is?
+  //  x=default_route[b];
+//    bitr |= (Gshift_[b][x]>>SRlength[x]) & 0x01; // or other logical opp for multiple bits/accum
+//    Gshift_[b][x]=(Gshift_[b][x]<<1)+bitr;
+//  } 
+  
   shift_[b]+= bitr;// & bitr;// ^ (!(GPIOC->IDR & 0x0010)); // or goes to 1s, xor is risky, AND works... and clockbit PC4
-  //  printf("%d\n",bitr);
-  print32bits(shift_[0]);
+  printf("%d: ",b);
+  print32bits(shift_[b]);
   //  print32bits(Gshift_[0][0]);
-  printf("\n");
+  //  printf("\n");
 
   // generic output for fake CLKINS on: LSRCLK-pulse9=PB12, RSRCLK-pulse10=PB13, CSRCLCK-pulse11=PB14
   // just deal with CLKS - they are all on PB. we can route to all/any so 3 bits
@@ -445,37 +470,46 @@ int main(void)   // try to re-learn pointer arrays
 
   
   }
+    c++;
+
   }
 
-  
   ////////////////////////////////////////////////////
   // how can we compare routing with ghosts to routing with coggs at different speeds
   ////////////////////////////////////////////////////
   // basic coggs code;
-
   /*
-  printf("coggs: \n");
- 
-  for (y=0;y<32;y++){
+  
+  uint32_t laster=0;
+  for (x=0;x<128000;x++){
+  
+    speedn=rand()%1280;
+    speedr=rand()%1280;
+    //  speedn=9;speedr=9;
+    SRlengthn=(rand()%28)+3; SRlengthr=(rand()%28)+3;
+
+    
+    //  printf("coggs:  ");
+    shift_registern=0xff; shift_registerr=0xff; countern=0; counterr=0; coggr=1;coggn=0; bitr=0; // now they are the same!
+  for (y=0;y<10241;y++){
   
   countern++;
   if (countern>=speedn){ 
     countern=0;
     bitn = ((shift_registern >> (lfsr_taps[SRlengthn][0])) ^ (shift_registern >> (lfsr_taps[SRlengthn][1])) ^ (shift_registern >> (lfsr_taps[SRlengthn][2])) ^ (shift_registern >> (lfsr_taps[SRlengthn][3]))) & 1u; // 32 is 31, 29, 25, 24
     // need to catch it
-    if (shift_registern==0)     shift_registern=0xff;
+    //    if (shift_registern==0)     shift_registern=0xff;
     
     shift_registern=shift_registern<<1; // we are shifting left << so bit 31 is out last one
     //    bitn |=bit;
     if (coggr==0)    shift_registern+= bitn | bitr;
     else shift_registern+= bitn | ((shift_registerr>>(SRlengthr-(coggr-1)))&0x01);
     coggr++;
-    if (coggr>(SRlengthr+1)) coggr=0; // we always update the cogg which is feeding into this one
+    if (coggr>(SRlengthr+1)) coggr=1; // we always update the cogg which is feeding into this one
     coggn=0;
-    print32bits(shift_registern);
   }
 
-  speedr=12;
+    
   counterr++;
   if (counterr>=speedr){
     counterr=0;
@@ -484,19 +518,22 @@ int main(void)   // try to re-learn pointer arrays
   coggr=0;
   }  
   } // 32 y
+  //  print32bits(shift_registern);
+  laster=shift_registern;
+  
+  //  printf("ghosts: ");
 
-  printf("ghosts: \n");
-
-  shift_registern=0xff; 
-
-  for (y=0;y<32;y++){
+  shift_registern=0xff; shift_registerr=0xff; countern=0; counterr=0;
+  Gshift_registerr=0xff;
+  
+  for (y=0;y<10241;y++){
       
   countern++;
   if (countern>=speedn){ 
     countern=0;
     bitn = ((shift_registern >> (lfsr_taps[SRlengthn][0])) ^ (shift_registern >> (lfsr_taps[SRlengthn][1])) ^ (shift_registern >> (lfsr_taps[SRlengthn][2])) ^ (shift_registern >> (lfsr_taps[SRlengthn][3]))) & 1u; // 32 is 31, 29, 25, 24
     // need to catch it
-    if (shift_registern==0)     shift_registern=0xff;
+    //    if (shift_registern==0)     shift_registern=0xff;
     // copy now to ghost
     Gshift_registern=shift_registern; // this could also be ORed or other logic operation with former ghost!
     shift_registern=shift_registern<<1; // we are shifting left << so bit 31 is out last one
@@ -504,25 +541,25 @@ int main(void)   // try to re-learn pointer arrays
     bitr=(Gshift_registerr>>SRlengthr) & 0x01; 
     Gshift_registerr=(Gshift_registerr<<1)+bitr; 
 
-    shift_registern+= bitn;// & bitr;// ^ (!(GPIOC->IDR & 0x0010)); // or goes to 1s, xor is risky, AND works... and clockbit PC4
-    print32bits(shift_registern);
+    shift_registern+= bitn | bitr;// & bitr;// ^ (!(GPIOC->IDR & 0x0010)); // or goes to 1s, xor is risky, AND works... and clockbit PC4
   }
 
-  speedr=12;
   counterr++;
   if (counterr>=speedr){
     counterr=0;
   bitr = (shift_registerr>>SRlengthr) & 0x01; // bit which would be shifted out but we don't use it so far
   Gshift_registerr=shift_registerr; // this could also be ORed or other logic operation with former ghost!
   //  bitr = (shift_registerr>>SRlengthr) & 0x01; // bit which would be shifted out but we don't use it so far
-  shift_registerr=(shift_registerr<<1)+bitr; // loop in this case
-
-  
+  shift_registerr=(shift_registerr<<1)+bitr; // loop in this case  
   }
   
   
-  
-    } // 32 ghosts
+  } // 32 ghosts
+
+  //      print32bits(shift_registern);
+  //      printf("n: %d r: %d\n", speedn, speedr);
+      if (shift_registern!=laster) printf("xxxxxxxxxxxxxxxxxx n: %d r: %d\n", speedn, speedr);
+  }
   */
   // XOR;
   //  int testy=1^0^0;
