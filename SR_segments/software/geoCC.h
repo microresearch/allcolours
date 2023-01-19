dbed354f74ac7c991143b2e39d88de36161a9a51
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-01-19 19:12:41 +0100

    xx

---
+++ SR_segments/software/geoCC.h
@@ -0,0 +1,43 @@
+//- TODO: fill in and simplify geoC according to 16/16/16/16 modes
+
+/// INNER
+
+void SR_geo_inner_fixedC(uint32_t w){  // fixed route only - no probability, no adc - this can be generic
+  HEADNADA;
+  if (interpfromnostrobe[gate[w].matrix[0]>>7]){ 
+    gate[w].alpha = gate[w].time_now - (float)gate[w].int_time;
+    gate[w].dac = ((float)delay_buffer[w][DELAY_SIZE-5] * gate[w].alpha) + ((float)delay_buffer[w][DELAY_SIZE-6] * (1.0f - gate[w].alpha));
+    if (gate[w].dac>4095) gate[w].dac=4095;
+  }
+  else gate[w].dac = delay_buffer[w][1];
+
+    if ((*speedfromnostrobe[gate[w].matrix[0]>>7])(gate[w].matrix[1], gate[w].matrix[2], w)){ // speedfunc
+      gate[w].fake=gate[w].trigger;
+    GSHIFT_;
+    SRlength[w]=lookuplenall[gate[w].matrix[6]>>7]; // why it makes difference if this is before or after...
+    bitn=binroutesel0(0,0,w); // now uses routetype
+    BITN_AND_OUTV_; 
+    new_data(val,w);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// OUTER
+
+void SR_geo_outer_route(uint32_t w){  // fixed route // the most basic but no reset
+  gate[w].matrix[0]=0<<7; // spdfrac
+  gate[w].matrix[1]=CV[w];//gate[dacfrom[daccount][w]].dac; // speed
+  gate[w].inner=SR_geo_inner_fixedC; // totally fixed 
+}
+
+// {0speedfrom/index, 1speedcv1, 2speedcv2, 3bit/index, 4bitcv1, 5bitcv2, 6lencv, 7adc, 8adccv, 9prob/index, 10probcv1, 11probvcv2, 12altfuncindex, 13dactype, 14dacpar, 15strobeindex
+
+void SR_geo_outer_C00(uint32_t w){  // set dactype, spdfrac, fixed route // RESETR - no need for changed
+  if (gate[w].changed==1) RESETC; // added 21/12 only reset on change 
+  gate[w].matrix[0]=0<<7; // spdfrac
+  gate[w].matrix[1]=CV[w];// speed
+  gate[w].matrix[13]=CVL[w]; // dactype
+  gate[w].inner=SR_geo_inner_fixedC; // fixedtype/globalroute
+}
+
+// fixed probs????
---
+++ SR_segments/software/newbits.h
@@ -0,0 +1,10 @@
+// new arrays of bit functions - deprecate all doubles with no types, fixedroutes, and sel2 - all as marked in segmodes...
+// divide as to strobe/no strobe
+
+// integrate exp_ports once tested...
+
+// allbits
+
+// routebits
+
+// abstract
