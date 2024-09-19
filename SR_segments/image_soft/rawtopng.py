b6204ab3a2827fd3536da0443686b7060e869546
root <root@debian-BULLSEYE-live-builder-AMD64>
2024-09-19 16:10:18 +0200

    xx

--- SR_segments/image_soft/rawtopng.py
+++ SR_segments/image_soft/rawtopng.py
@@ -3,12 +3,12 @@ from PIL import Image
 
 f = open("test2", "rb")  # reopen the file
 data=f.read()
-y=1195
+y=1000
 #for x in range(0,1024):
 #first 54 bytes remove
 #data=data[1:]
-x=1000
+x=1024
 #img = Image.frombuffer('RGB', (1024,y), data) # was 512+x
-img = Image.frombuffer('L', (1000,y), data) # was 512+x
+img = Image.frombuffer('L', (1024,y), data) # was 512+x
 filename="test"+str(x)+".png"
 img.save(filename)
